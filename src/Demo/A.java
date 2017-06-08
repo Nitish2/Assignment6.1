0b1b5e00ff442a932b581f4d8cba13aa3a0f9b4a
Nitish2 <nitishn886@gmail.com>
2017-06-08 14:44:45 +0530

    Assignment6.1

---
+++ src/Demo/A.java
@@ -0,0 +1,37 @@
+package Demo;
+
+/**
+ * We can make only one public class in a single program.
+ * File will be save from the name of Public class . ( Class A )
+ * @author Hp
+ *
+ */
+public class A {
+
+	public static void main(String[] args) {
+		System.out.println("Name:- Nitish Negi");
+	}
+
+}
+
+class B {
+	public static void main(String[] args) {
+		System.out.println("Age:- 24");
+	}
+
+}
+
+class C {
+
+	public static void main(String[] args) {
+	 System.out.println("Course:-"+"Core java +"+" Android Development");
+	}
+}
+
+
+class D {
+	public static void main(String[] args) {
+		System.out.println("Rank:- 2");
+	}
+
+}
